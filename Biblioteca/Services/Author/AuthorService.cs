4cd2f3e9217be8f874851848cdbf70a32b45abad
Salvatore Chiricosta <chiricosta.salvatore27@gmail.com>
2024-06-12 22:04:30 +0200

    Aggiunti mapper e servizi con metodi di aggiunta ed eccezione per autore

--- Biblioteca/Services/Author/AuthorService.cs
+++ Biblioteca/Services/Author/AuthorService.cs
@@ -1,13 +1,31 @@
-﻿using Biblioteca.Models.DTO.Authors;
+﻿using Biblioteca.DTO.Authors;
+using Biblioteca.Models;
 using Biblioteca.Repository;
 
 namespace Biblioteca.Services.Author
 {
     public class AuthorService : IAuthorService
     {
+        public IAuthorMapper _mapper;
+
+        public AuthorService(IAuthorMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public AuthorDTO CreateAuthor(CreateAuthorRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using var context = new LibraryDbContext();
+                var authorToAdd = _mapper.ToAuthorEntity(request);
+                context.Add(authorToAdd);
+                context.SaveChanges();
+                return _mapper.ToAuthorDTO(authorToAdd);
+            }
+            catch (Exception)
+            {
+                throw new Exceptions.Authors.CantAddAuthorException();
+            }
         }
 
         public DeleteAuthorResponse DeleteAuthor(string id)
